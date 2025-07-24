c6cf8aa7a2684c888fb184bb2814b3cd57846f8d
Prashanth_reddy <109143867+prash1reddy@users.noreply.github.com>
2025-07-24 17:26:06 +0530

    Update CaseStudy.java

--- Selenium Launch Different Browser/src/edgeSelenium/CaseStudy.java
+++ Selenium Launch Different Browser/src/edgeSelenium/CaseStudy.java
@@ -10,30 +10,26 @@ public class CaseStudy {
 	        WebDriver driver = new EdgeDriver();
 	        driver.manage().window().maximize();
 
-	        // 1. OrangeHRM
 	        driver.get("https://opensource-demo.orangehrmlive.com/");
 	        System.out.println(driver.getTitle());
 	        System.out.println(driver.getCurrentUrl());
 	        System.out.println(driver.getPageSource());
 
-	        // 2. OpenCart
 	        driver.get("https://demo.opencart.com/");
 	        System.out.println(driver.getTitle());
 	        System.out.println(driver.getCurrentUrl());
 	        System.out.println(driver.getPageSource());
 
-	        // 3. Bing
 	        driver.get("http://www.bing.com");
 	        System.out.println(driver.getTitle());
 	        System.out.println(driver.getCurrentUrl());
 	        System.out.println(driver.getPageSource());
 
-	        // 4. FreeCRM
 	        driver.get("https://www.freecrm.com/");
 	        System.out.println(driver.getTitle());
 	        System.out.println(driver.getCurrentUrl());
 	        System.out.println(driver.getPageSource());
-	        // 5. Half eBay
+	        
 	        driver.get("http://www.half.ebay.com");
 	        System.out.println(driver.getTitle());
 	        System.out.println(driver.getCurrentUrl());
